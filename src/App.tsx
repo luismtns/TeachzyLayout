f084026c21ba0133361f2ed96cad4a407d20bf97
Luis Bovo <luis.bovo@bit.com.br>
2020-11-02 04:52:32 -0300

    rename sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ScreenClassProvider, Col, Container, Row } from "react-grid-system";
 import "./App.css";
 import CourseView from "./components/CourseView/index";
 import Header from "./components/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
+import Sidebar from "./components/sidebar/sidebar";
 
 function App() {
   return (
