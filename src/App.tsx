79ee26fd08520cc986c606d196ef2b969ecca871
Luis Bovo <luis.bovo@bit.com.br>
2020-11-02 05:01:37 -0300

    all indexed

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { ScreenClassProvider, Col, Container, Row } from "react-grid-system";
 import "./App.css";
 import CourseView from "./components/CourseView/index";
-import Header from "./components/Header/Header";
-// import Sidebar from "./components/Sidebar/Sidebar";
+import Header from "./components/Header";
+import Sidebar from "components/Sidebar";
 
 function App() {
   return (
     <ScreenClassProvider>
       <Container fluid>
         <Row>
-          <Col md={3}>{/* <Sidebar /> */}</Col>
+          <Col md={3}>
+            <Sidebar />
+          </Col>
           <Col md={4}>
             <CourseView />
           </Col>
